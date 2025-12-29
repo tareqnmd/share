8778572002ff4e14b6bf24e653dda8c49f70ca46
Tareq <md.tareq@asthait.com>
2025-12-29 11:23:21 +0600

    Refactor SignIn component and update GoogleIcon for improved styling and accessibility
    - Replaced the button element with a custom Button component, enhancing styling and accessibility.
    - Updated GoogleIcon to use a new SVG format for better scalability and visual consistency.
    - Adjusted button properties for improved user experience during sign-in.

--- components/shared/SignIn.tsx
+++ components/shared/SignIn.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import { GoogleIcon } from '@/components/icons';
+import Button from '@/components/ui/Button';
 import { AuthProvider } from '@/enums/auth-provider.enum';
+import { ButtonSize } from '@/enums/button-size.enum';
+import { ButtonVariant } from '@/enums/button-variant.enum';
 import { signIn } from 'next-auth/react';
 
 const SignIn = () => {
 	return (
-		<button
+		<Button
+			variant={ButtonVariant.SECONDARY}
+			size={ButtonSize.MD}
 			onClick={() => signIn(AuthProvider.GOOGLE)}
-			className="flex items-center gap-2 px-4 py-2 text-sm font-medium bg-neutral-100 text-neutral-900 rounded-lg hover:bg-neutral-200 transition-colors cursor-pointer focus:outline-none focus:ring-2 focus:ring-neutral-500/30"
+			aria-label="Sign in"
+			className="px-6! py-2.5!"
 		>
 			<GoogleIcon className="w-4 h-4" />
-			<span>Sign In with Google</span>
-		</button>
+			<span className="text-sm font-medium leading-none">Sign In</span>
+		</Button>
 	);
 };
 
